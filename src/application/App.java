64311b2ca3b101043435e5a67a75a796f70301b4
Vitor Hugo De Lima Moreira <vitor.moreira@UNIFACEAR.ADM>
2026-04-13 20:09:57 -0300

    Esboço das classes criadas

---
+++ src/application/App.java
@@ -0,0 +1,9 @@
+package application;
+
+import entities.Livro;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ src/entities/Usuario.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Usuario {
+    String nome;
+    int idade;
+
+    public Usuario(){
+    }
+    public Usuario(String nome, int idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+
+    
+
+}
